6c9a0a2ca8c47de2670e32fd31876b9f24bd707e
williamzmacdonald <33135172+williamzmacdonald@users.noreply.github.com>
2022-04-18 03:46:05 -0500

    Add prisma adapter for nextauth and schema

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../../../db/index";
 
 export default NextAuth({
     providers: [
@@ -11,4 +13,5 @@ export default NextAuth({
     debug: true,
     // debug: process.env.NODE_ENV === "development",
     secret: process.env.AUTH_SECRET,
+    adapter: PrismaAdapter(prisma),
 });
